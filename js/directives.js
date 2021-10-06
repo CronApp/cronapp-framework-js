5d382f21f71cea631fb743ba9c20b52b992a10ec
JaquelineCronapp <jaqueline.peixer@cronapp.io>
2021-10-06 09:27:58 -0300

    desenvolvimento 5-10

--- js/directives.js
+++ js/directives.js
@@ -4580,8 +4580,8 @@
           require: 'ngModel',
           link: function(scope, elem, attrs, ngModelCtrl) {
 
-            attrs.theme = $(elem).find('i').attr('xattr-theme');
-            attrs.iconOn = $(elem).find('i').attr('class');
+            // attrs.theme = $(elem).find('i').attr('xattr-theme');
+            // attrs.iconOn = $(elem).find('i').attr('class');
 
             var $elem = $(elem);
             var starArray = [];
@@ -4621,13 +4621,13 @@
               for (var i=1;i<=5;i++) {
                 stars[i-1].removeClass(attrs.iconOff || 'fa fa-star-o');
                 stars[i-1].removeClass(attrs.iconOn);
-                stars[i-1].removeClass(attrs.theme);
+                // stars[i-1].removeClass(attrs.theme);
                 if (i <= value) {
                   stars[i-1].addClass(attrs.iconOn);
                   stars[i-1].addClass(attrs.theme);
                 } else {
                   stars[i-1].addClass(attrs.iconOff || 'fa fa-star-o');
-                  stars[i-1].addClass(attrs.theme);
+                  // stars[i-1].addClass(attrs.theme);
                 }
               }
               return value;
@@ -4754,7 +4754,7 @@
               //Somente fica na vertical se for o menu principal da IDE (E estiver configurado para isso)
               let isVertical = element.closest('.crn-navigator-vertical').length;
   
-              var TEMPLATE_MAIN = '<ul  class="nav navbar-nav ' + (isVertical ? 'col-md-12 padding-0' : '') + ' " style="float:left"></ul>';
+              var TEMPLATE_MAIN = '<ul  class="nav navbar-nav ' + (isVertical ? 'col-md-12 padding-0' : '') + ' "></ul>';
               var options = {};
               try {
                 options = JSON.parse(attrs.options);
