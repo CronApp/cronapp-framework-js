02ba477e4973be08e30e52fa34a623c52498262d
Jaqueline Peixer <79110134+JaquelineCronapp@users.noreply.github.com>
2021-10-21 14:36:40 -0300

    CRONAPP-4590 - Ajustes na inserção dos componentes no editor (#720)
    * ajustes 1
    
    * desenvolvimento 5-10
    
    * CRONAPP-4590 - Ajustes na inserção dos componentes no editor

--- js/directives.js
+++ js/directives.js
@@ -4596,11 +4596,15 @@
           require: 'ngModel',
           link: function(scope, elem, attrs, ngModelCtrl) {
 
-            attrs.theme = $(elem).find('i').attr('xattr-theme');
-            attrs.iconOn = $(elem).find('i').attr('class');
-
             var $elem = $(elem);
             var starArray = [];
+
+            var icon = $elem.find('i');
+
+            attrs.iconSize = icon[0].dataset.size;
+            attrs.iconTheme = icon[0].dataset.theme;
+            attrs.iconOn = icon[0].className;            
+            attrs.iconOn = attrs.iconOn.replace(attrs.iconSize).replace(attrs.iconTheme);            
 
             if (attrs.xattrDefaultValue) {
               ngModelCtrl.$viewValue = 0; //set new view value
@@ -4609,6 +4613,7 @@
 
             for (var i=1;i<=5;i++) {
               starArray.push($(elem).find('i').get(i - 1));
+              $(starArray[i-1]).removeClass(attrs.iconOn);
               $(starArray[i-1]).addClass(attrs.iconOff || "fa fa-star-o");
             }
 
@@ -4637,13 +4642,16 @@
               for (var i=1;i<=5;i++) {
                 stars[i-1].removeClass(attrs.iconOff || 'fa fa-star-o');
                 stars[i-1].removeClass(attrs.iconOn);
-                stars[i-1].removeClass(attrs.theme);
+                stars[i-1].removeClass(attrs.iconSize);
+                stars[i-1].removeClass(attrs.iconTheme);
                 if (i <= value) {
                   stars[i-1].addClass(attrs.iconOn);
-                  stars[i-1].addClass(attrs.theme);
+                  stars[i-1].addClass(attrs.iconSize);
+                stars[i-1].addClass(attrs.iconTheme);
                 } else {
                   stars[i-1].addClass(attrs.iconOff || 'fa fa-star-o');
-                  stars[i-1].addClass(attrs.theme);
+                  stars[i-1].addClass(attrs.iconSize);
+                stars[i-1].addClass(attrs.iconTheme);
                 }
               }
               return value;
@@ -4770,7 +4778,7 @@
               //Somente fica na vertical se for o menu principal da IDE (E estiver configurado para isso)
               let isVertical = element.closest('.crn-navigator-vertical').length;
   
-              var TEMPLATE_MAIN = '<ul  class="nav navbar-nav ' + (isVertical ? 'col-md-12 padding-0' : '') + ' " style="float:left"></ul>';
+              var TEMPLATE_MAIN = '<ul  class="nav navbar-nav ' + (isVertical ? 'col-md-12 padding-0' : '') + ' "></ul>';
               var options = {};
               try {
                 options = JSON.parse(attrs.options);
