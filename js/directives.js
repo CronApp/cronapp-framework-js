fc8a914777ce46ca2b0f97260cd69fefdb4b80ef
Jaqueline Peixer <79110134+JaquelineCronapp@users.noreply.github.com>
2021-12-28 10:04:14 -0300

    CRONAPP-5018 - Falta propriedade de habilitar/desabilitar componente (#810)
    * CRONAPP-5018 - Falta propriedade de habilitar/desabilitar componente

--- js/directives.js
+++ js/directives.js
@@ -3512,7 +3512,7 @@
             var parent = element.parent();
             $(parent).append('<input style="width: 100%;" '+ id + name + ' class="cronSelect"/>');
             var $element = $(parent).find('input.cronSelect');
-
+            var cronDisabled = attrs.cronDisabled ? attrs.cronDisabled : 'true';
             var options = await app.kendoHelper.getConfigCombobox(select, scope);
             options.close = attrs.ngClose ? function (){scope.$eval(attrs.ngClose)}: undefined;
             options.dataBound = attrs.ngDataBound ? function (){scope.$eval(attrs.ngDataBound)}: undefined;
@@ -3562,7 +3562,11 @@
               }
             };
             initializeAttrAndEvents();
-            combobox.enable(true);
+            if (cronDisabled) {
+              combobox.enable(false);
+            } else {
+              combobox.enable(true);
+            }
 
             var _scope = scope;
             var _ngModelCtrl = ngModelCtrl;
@@ -3663,6 +3667,7 @@
             var self = this;
             var parentDS = {};
             var textField = null;
+            var cronDisabled = attrs.cronDisabled ? attrs.cronDisabled : 'true';
             try {
               select = JSON.parse(attrs.options);
               parentDS = this.getActive(attrs.ngModel);
@@ -4026,7 +4031,13 @@
             _compileAngular(scope, combobox.ul);
 
             $(element).remove();
-            combobox.enable(true);
+
+            if(cronDisabled){
+              combobox.enable(false);
+            } else {
+              combobox.enable(true);
+            }
+
             $("[aria-owns='" + `${attrs.id}_listbox` + "']").attr('aria-label', $translate.instant('template.crud.search'));
           }
         };
@@ -4040,6 +4051,7 @@
             var modelGetter = $parse(attrs['ngModel']);
             var modelSetter = modelGetter.assign;
             var model = attrs['ngModel'];
+            var cronDisabled = attrs.cronDisabled ? attrs.cronDisabled : 'true';
 
             var _self = this;
             var select = {};
@@ -4176,7 +4188,11 @@
             deselect = attrs.ngDeselect ? function (){_scope.$eval(attrs.ngDeselect)}: undefined;
 
             var combobox = $element.kendoMultiSelect(options).data('kendoMultiSelect');
-            combobox.enable(true);
+            if(cronDisabled){
+              combobox.enable(false);
+            }else {
+              combobox.enable(true);
+            }
             app.kendoHelper.receivePushData(combobox);
 
             $("[aria-describedby='" + `${attrs.id}_taglist` + "']").attr('id', `${attrs.id}-container`);
@@ -4246,6 +4262,7 @@
             options.select = attrs.ngSelect ? function (){scope.$eval(attrs.ngSelect);}: undefined;
             var parent = element.parent();
             var id = attrs.id ? ' id="' + attrs.id + '"' : '';
+            var cronDisabled = attrs.cronDisabled ? attrs.cronDisabled : 'true';
             var name = attrs.name ? ' name="' + attrs.name + '"' : '';
             var validationmessage = attrs.validationmessage ? ' validationmessage="' + attrs.validationmessage + '"' : '';
             var required = '';
@@ -4277,7 +4294,12 @@
             }
 
             var autoComplete = $element.kendoAutoComplete(options).data('kendoAutoComplete');
-            autoComplete.enable(true);
+
+            if(cronDisabled){
+              autoComplete.enable(false);
+            } else {
+              autoComplete.enable(true);
+            }            
 
             if (ngModelCtrl) {
               ngModelCtrl.$formatters.push(function (value) {
